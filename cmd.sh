ab693fb1610ca5699841ae0ed71f3b8f6306e6ac
RobertSamoilescu <robert.samoilescu@gmail.com>
2019-04-06 00:17:16 +0300

    cmd change

--- cmd.sh
+++ cmd.sh
@@ -1,2 +1,2 @@
 # python3 -m scripts.train --algo ppo --model CarRacingDQNStacked4 --save-interval 50 --procs 12 --tb 1>out 2>err
-python3 -m scripts.train --algo ppo --model CarRacing --save-interval 10 --procs 1 --tb --recurrence 64
+python3 -m scripts.train --algo ppo --model CarRacingGRU --save-interval 10 --procs 8 --tb --recurrence 64 1>out 2>err
